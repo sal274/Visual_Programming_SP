c746e79d415509daacd9ffc34048be38946ccb82
sal274 <88658263+sal274@users.noreply.github.com>
2024-05-08 03:09:07 +0500

    Update comments, and other minor stuff

--- WriteOn/Shared/CurrentUserState.cs
+++ WriteOn/Shared/CurrentUserState.cs
@@ -2,7 +2,7 @@ namespace WriteOn.Shared;
 
 class CurrentUserState
 {
-    public int UserId { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public int UserId { get; set; } = 0;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
