c44516c4ea670d697e135618e4963e8e5754e57e
sal274 <88658263+sal274@users.noreply.github.com>
2024-05-16 15:49:44 +0500

    Add gitignore

--- WriteOn/obj/Debug/net8.0/WriteOn.AssemblyInfo.cs
+++ WriteOn/obj/Debug/net8.0/WriteOn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WriteOn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f191f598ca1fa5b3f72148a34b90d13fb1220228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ff446c22f8dd663c2d5fdd8128a633283d9cc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WriteOn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WriteOn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
