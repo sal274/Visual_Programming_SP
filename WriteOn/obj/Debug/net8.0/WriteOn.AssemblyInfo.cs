5d5ae2b8f2b18021b0b07481b86d793af693e5bf
sal274 <88658263+sal274@users.noreply.github.com>
2024-05-18 12:59:51 +0500

    Add separate article page

--- WriteOn/obj/Debug/net8.0/WriteOn.AssemblyInfo.cs
+++ WriteOn/obj/Debug/net8.0/WriteOn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WriteOn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0713d4b728010aff4115bf7498461a0de14b819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167ee77ce750793e76395f6704b1ed0d6e9f8936")]
 [assembly: System.Reflection.AssemblyProductAttribute("WriteOn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WriteOn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
